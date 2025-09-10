67b08c0241b292d30579e573ef8cd6bd0ce7c743
plt1-coder <p.andreoficial@gmail.com>
2025-09-10 17:07:22 +0000

    criei o desafio

---
+++ super_trunfo.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main(){
+
+// Carta 1 - Ceara
+ 
+char Estado[20] = "C";
+char Codigo[20] = "C01";
+char Nome_da_cidade[20] = "Fortaleza";
+int população = 2428708;
+float area = 312.353;
+float PIB = 27.16445;
+int Número_de_pontos_turisticos = 14;
+
+
+printf("===== CARTA 1 =====\n");
+
+
+printf("Estado: %s\n", Estado);
+printf("Codigo: %s\n", Codigo);
+printf("Nome da cidade: %s\n", Nome_da_cidade);
+printf("População: %d\n", população);
+printf("Área: %f\n", area);
+printf("PIB: %f\n", PIB);
+printf("Número de pontos turisticos: %d\n\n", Número_de_pontos_turisticos);
+
+
+
+// Carta 2
+
+printf("===== CARTA 2 =====\n");
+
+
+
+char estado[20] = "S";
+char codigo[20] = "S02";
+char nomecidade[20] = "Florianópolis";
+int populacao = 537211;
+float Area = 674.844;
+float pib = 45602.98;
+int numerodepontos = 30;
+
+
+printf("Estado: %s\n", estado);
+printf("Código: %s\n", codigo);
+printf("Nome da cidade: %s\n", nomecidade);
+printf("População: %d\n", populacao);
+printf("Área: %f\n", Area);
+printf("PIB: %f\n", pib);
+printf("Número de pontos turisticos: %d\n", numerodepontos);
+
+
+
+
+
+}
+
+
